2ffee722c3ca1d477637816825ac350379b91d5f
durga-206 <durga26604@gmail.com>
2025-09-06 19:12:04 +0530

    feat: adding normal if to fuction

--- Easy_problem_solving/even.py
+++ Easy_problem_solving/even.py
@@ -1,7 +1,16 @@
-li=input().split()
+'''li=input().split()
 even=list()
 for i in li:
     if int(i)%2==0:
         even.append(int(i))
-print(even)
-        
+print(even)'''
+#using function
+def evens(li):
+    even=list()
+    for i in li:
+        if int(i)%2==0:
+          even.append(int(i))
+    return even
+li=input().split()
+print(evens(li))
+
--- Easy_problem_solving/even_sum.py
+++ Easy_problem_solving/even_sum.py
@@ -1,6 +1,15 @@
-li=input().split()
+'''li=input().split()
 add=0
 for i in li:
     if int(i)%2==0:
         add+=int(i)
-print(add)
+print(add)'''
+#using function
+def even_sum(li):
+    even_add=0
+    for i in li:
+        if int(i)%2==0:
+            even_add+=int(i)
+    return even_add
+li=input().split()
+print(even_sum(li))
